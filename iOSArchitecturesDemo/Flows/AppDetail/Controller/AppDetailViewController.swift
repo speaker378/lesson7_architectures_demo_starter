76985e09c4b06f9687368bdb30c7f1ed17531fbe
speaker378 <speaker378debua@gmail.com>
2022-02-26 23:34:08 +0500

    Task 1

--- iOSArchitecturesDemo/Flows/AppDetail/Controller/AppDetailViewController.swift
+++ iOSArchitecturesDemo/Flows/AppDetail/Controller/AppDetailViewController.swift
@@ -12,6 +12,7 @@ final class AppDetailViewController: UIViewController {
     
     public var app: ITunesApp
     private lazy var headerViewController = AppDetailHeaderViewController(app: app)
+    private lazy var whatIsNewViewController = WhatIsNewViewController(app: app)
     
     private let imageDownloader = ImageDownloader()
     
@@ -29,19 +30,10 @@ final class AppDetailViewController: UIViewController {
     }
     
     // MARK: - Lifecycle
-    
-//    override func loadView() {
-//        super.loadView()
-//        self.view = AppDetailView()
-//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configureUI()
-        
-//        self.configureNavigationController()
-//        self.downloadImage()
     }
     
     
@@ -53,7 +45,7 @@ final class AppDetailViewController: UIViewController {
         self.navigationItem.largeTitleDisplayMode = .never
         
         addHeaderViewController()
-        addDescriptionViewController() //ДЗ
+        addWhatIsNewViewController()
     }
     
     private func addHeaderViewController() {
@@ -65,44 +57,23 @@ final class AppDetailViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             headerViewController.view.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-//            headerViewController.view.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
             headerViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor),
             headerViewController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor)
         ])
         
     }
     
-    private func addDescriptionViewController() {
-        //ДЗ: Добавить другие модели
+    private func addWhatIsNewViewController() {
+        self.addChild(whatIsNewViewController)
+        self.view.addSubview(whatIsNewViewController.view)
         
-        let descriptionViewController = UIViewController()
-        
-        self.addChild(descriptionViewController)
-        self.view.addSubview(descriptionViewController.view)
-        
-        descriptionViewController.didMove(toParent: self)
-        descriptionViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        whatIsNewViewController.didMove(toParent: self)
+        whatIsNewViewController.view.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-//            descriptionViewController.view.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            descriptionViewController.view.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
-            descriptionViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-            descriptionViewController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor)
+            whatIsNewViewController.view.topAnchor.constraint(equalTo: headerViewController.view.bottomAnchor),
+            whatIsNewViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            whatIsNewViewController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor)
         ])
     }
-    
-//    private func configureNavigationController() {
-//        self.navigationController?.navigationBar.tintColor = UIColor.white;
-//        self.navigationItem.largeTitleDisplayMode = .never
-//    }
-//
-//    private func downloadImage() {
-//        guard let url = self.app?.iconUrl else { return }
-//        self.appDetailView.throbber.startAnimating()
-//        self.imageDownloader.getImage(fromUrl: url) { (image, error) in
-//            self.appDetailView.throbber.stopAnimating()
-//            guard let image = image else { return }
-//            self.appDetailView.imageView.image = image
-//        }
-//    }
 }
